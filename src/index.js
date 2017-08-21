e49c50985d52029ecf145eb1fff37b598a89efaf
Jinx <Rapture@darkAngel.local>
2017-08-21 12:17:26 -0700

    global method works, found click target

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 // const today = Date.now();
 // eslint-disable-next-line no-console
 // console.log(`Everything is working!\n${today}\nYay! 😁🎉`);
+
+const timerList = document.querySelectorAll('.time'); // our timer array
+const timerArr = Array.from(timerList);
+
+console.log(timerArr);
+
 const timer = (sec, timerNum) => {
   let timerEl = document.querySelector(`#t${timerNum}`); // get our specific timer
   let secInt = sec;
@@ -15,7 +21,25 @@ const timer = (sec, timerNum) => {
     clearTimeout(ourTimer);
     return;
   }
+
+  // const handleClick = (e) => {
+  //   console.log(this);
+  //   console.log(e);
+  // };
+  //
   secInt = secInt - 1;
+};
+
+const timerListeningArr = timerArr.map((el, index) => {
+  el.addEventListener('click', (e) => {
+    console.log(e.target.id);
+    console.log(e);
+  });
+  return el;
+});
+
+window.setTimer = (dur, el) => {
+  return timer(dur, el);
 };
 
 timer(60, 1);
@@ -25,11 +49,11 @@ setTimeout(() => {
 },200);
 
 setTimeout(() => {
-  timer(60, 3);
+  timer(40, 3);
 },500);
 
 setTimeout(() => {
-  timer(80, 4);
+  timer(55, 4);
 },1000);
 
 setTimeout(() => {
